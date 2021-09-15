e89b335e837e28be00495f5640fe71604414dca5
Batchu Sai Akhil <batchusai.akhil@mrcooper.com>
2021-09-15 14:26:19 +0530

    fixed second time file upload issue

--- src/client/components/common/FileUploader/index.js
+++ src/client/components/common/FileUploader/index.js
@@ -4,29 +4,34 @@ import Button from '@material-ui/core/Button';
 //Sample Usage
 //<FileUploader onFileUpload={callBackToStoreFileData} buttonName="FileUpload"
 //style={classes.Style} acceptedFormat=".jpg,.png"/>
-const FileUploader = ({onFileUpload,buttonName,style,acceptedFormat=".jpg,.png,.pdf,.docx,.doc,.jpeg"}) => {
-  const hiddenFileInput = React.useRef(null);
+const FileUploader = ({ onFileUpload, buttonName, style, acceptedFormat = ".jpg,.png,.pdf,.docx,.doc,.jpeg" }) => {
+  let hiddenFileInput = React.useRef(null);
 
-  const handleClick = ()  => {
+  const handleClick = () => {
     hiddenFileInput.current.click();
   };
 
   const handleChange = event => {
-    const selectedFile = event.target.files[0];
-    const fileSize = selectedFile.size/1024/1024;
-    const formats = acceptedFormat.split(",")
-    let  errorMessage = ""
-    if(!formats.includes(getExtension(selectedFile.name))){
+    try {
+      const selectedFile = event.target.files[0];
+      const fileSize = selectedFile.size / 1024 / 1024;
+      const formats = acceptedFormat.split(",")
+      let errorMessage = ""
+      if (!formats.includes(getExtension(selectedFile.name))) {
         errorMessage = "File format not supported.Please upload a valid file"
-    }
-    if(fileSize > 10){
+      }
+      if (fileSize > 10) {
         errorMessage = "Please Upload file less than 10mb"
+      }
+      onFileUpload(selectedFile, event.target.value, event, errorMessage);
+      event.target.value = null;
+    } catch (error) {
+      console.log('error', error);
     }
-    onFileUpload(selectedFile,event.target.value,event,errorMessage);
   };
   return (
     <>
-      <Button onClick={handleClick}  className={style}  >
+      <Button onClick={handleClick} className={style}  >
         {buttonName}
       </Button>
       <input
@@ -34,14 +39,14 @@ const FileUploader = ({onFileUpload,buttonName,style,acceptedFormat=".jpg,.png,.
         type="file"
         ref={hiddenFileInput}
         onChange={handleChange}
-        style={{display: 'none'}}
+        style={{ display: 'none' }}
         accept={acceptedFormat}
       />
     </>
   );
 }
 
-function getExtension(file){
-    return file.substring(file.lastIndexOf('.')+1, file.length) || file
+function getExtension(file) {
+  return file.substring(file.lastIndexOf('.') + 1, file.length) || file
 }
 export default FileUploader
