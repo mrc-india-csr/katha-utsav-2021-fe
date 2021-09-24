e4b1e1b64acf029f56af1b634155b5f4e7267c55
HemanthaRajan <Hemantharajan.V1@mrcooper.com>
2021-09-24 18:39:48 +0530

    remove new tab

--- src/client/components/common/Button/DropDownButton.js
+++ src/client/components/common/Button/DropDownButton.js
@@ -99,7 +99,7 @@ export default function DropDownButton(props) {
                                             <MenuItem
                                                 key={option.name}
                                                 style={{fontSize: matchesXL?"2rem": matchesLG?"1.5rem":"inherit", paddingLeft: matchesXL?"1.5rem":matchesLG?"1.25rem":"1.8rem", paddingRight:matchesXL?"1.5rem":matchesLG?"1.25rem":"1.8rem"}}
-                                                onClick={() => {window.open(option.redirectPath,'_blank')}}
+                                                onClick={() => {window.open(option.redirectPath)}}
                                             >
                                                 {option.name}
                                             </MenuItem>
