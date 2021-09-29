37a7c60a742a8241fe1d51b86d4c50ab86954b2c
HemanthaRajan <Hemantharajan.V1@mrcooper.com>
2021-09-29 17:28:58 +0530

    Final schedule changes and personal link adding

--- src/client/components/Schedule/Schedule.jsx
+++ src/client/components/Schedule/Schedule.jsx
@@ -14,12 +14,8 @@ const useStyles = makeStyles(theme => ({
         }
     },
     header: {
-        fontSize: "48px",
-        color: "#18191F",
-        paddingTop: "86px",
-        [theme.breakpoints.down("xs")]: {
-            paddingTop: "42px",
-        }
+        paddingTop: "48px",
+        marginBottom: "0",
     },
     description: {
         fontFamily: "Poppins",
@@ -46,59 +42,70 @@ const useStyles = makeStyles(theme => ({
 
 const Events = [
     {
-        date: "24<sup>th</sup> Sept - 25<sup>th</sup> Oct",
+        date: "30<sup>th</sup> Sep - 25<sup>th</sup> Oct",
         phase: "Stage 1 : Upload Story",
-        text: "Registrations"
+        text: "Registrations",
+        singleDate: false
     },
     {
         date: "30<sup>th</sup> Oct",
         phase: "Stage 1 : Upload Story ",
-        text: "Result"
+        text: "Result",
+        singleDate: true
     },
     {
         date: "31<sup>st</sup> Oct - 15<sup>th</sup> Nov",
         phase: "Stage 2 : Know your Story",
-        text: "Registrations"
+        text: "Registrations",
+        singleDate: false
     },
     {
         date: "20<sup>th</sup> Nov - 21<sup>st</sup> Nov",
         phase: "Stage 2 : Know your Story",
-        text: "Session 1 - Session 2"
+        text: "Session 1 - Session 2",
+        singleDate: false
     },
     {
         date: "27<sup>th</sup> Nov - 28<sup>th</sup> Nov",
         phase: "Stage 2 : Know your Story",
-        text: "Session 3 - Session 4"
+        text: "Session 3 - Session 4",
+        singleDate: false
     },
     {
         date: "30<sup>th</sup> Nov",
         phase: "Stage 2 : Know your Story",
-        text: "Story Results"
+        text: "Story Results",
+        singleDate: true
     },
     {
         date: "30<sup>th</sup> Nov - 9<sup>th</sup> Dec",
         phase: "Stage 3 : Be the Writer",
-        text: "Registrations"
+        text: "Registrations",
+        singleDate: false
     },
     {
         date: "11<sup>th</sup> Dec - 12<sup>th</sup> Dec",
         phase: "Stage 3 : Be the Writer",
-        text: "Session 1 - Session 2"
+        text: "Session 1 - Session 2",
+        singleDate: false
     },
     {
         date: "18<sup>th</sup> Dec - 19<sup>th</sup> Dec",
         phase: "Stage 3 : Be the Writer",
-        text: "Session 3 - Session 4"
+        text: "Session 3 - Session 4",
+        singleDate: false
     },
     {
         date: "26<sup>th</sup> Dec - 27<sup>th</sup> Dec",
         phase: "Stage 3 : Be the Writer",
-        text: "Session 5 - Session 6"
+        text: "Session 5 - Session 6",
+        singleDate: false
     },
     {
         date: "8<sup>th</sup> Jan",
         phase: "Celebrations!!",
-        text: "Challenge Grounds Results & Valedictory Ceremony"
+        text: "Challenge Grounds Results & Valedictory Ceremony",
+        singleDate: true
     }
 ]
 
@@ -110,7 +117,7 @@ const Schedule = () => {
     return (
         <React.Fragment>
             <div id="schedule" className={classes.schedule}>
-                <div className={classes.header}>Schedule</div>
+                <h3 className={classes.header}>Schedule</h3>
                 <div className={classes.events}>
                     {Events.map((event)=>{
                         return(
@@ -119,7 +126,6 @@ const Schedule = () => {
                     })}
                 </div>
                 <div className={classes.description}>
-                    {/*<b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.Lorem ipsum dolor si*/}
                 </div>
             </div>
         </React.Fragment>
